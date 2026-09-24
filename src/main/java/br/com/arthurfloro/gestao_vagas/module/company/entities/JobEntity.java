e40751034be84a68fa67b6fb306feb608106443e
ArthurFloro <arthur.floro2006@gmail.com>
2026-09-24 07:59:15 -0300

    Entidade job

---
+++ src/main/java/br/com/arthurfloro/gestao_vagas/module/company/entities/JobEntity.java
@@ -0,0 +1,29 @@
+package br.com.arthurfloro.gestao_vagas.module.company.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
