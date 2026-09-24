a78147543b36c1aa93676518c22650ff3c5c08a0
ArthurFloro <arthur.floro2006@gmail.com>
2026-09-24 09:44:38 -0300

    removendo o name do parametro do RequestMapping

--- src/main/java/br/com/arthurfloro/gestao_vagas/module/company/controller/JobController.java
+++ src/main/java/br/com/arthurfloro/gestao_vagas/module/company/controller/JobController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/job")
+@RequestMapping("/job")
 public class JobController {
 
     @Autowired
