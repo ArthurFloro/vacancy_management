3f5ded9ea263067647f7021dbb2249e9d5e14d57
ArthurFloro <arthur.floro2006@gmail.com>
2026-09-25 14:56:58 -0300

    Criando token de candidato

---
+++ src/main/java/br/com/arthurfloro/gestao_vagas/module/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,4 @@
+package br.com.arthurfloro.gestao_vagas.module.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+}
---
+++ src/main/java/br/com/arthurfloro/gestao_vagas/module/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.arthurfloro.gestao_vagas.module.candidate.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCandidateResponseDTO {
+    private String access_token;
+}
