910836e412582a6c90db7eb2bf09016d95cd3d4b
ArthurFloro <arthur.floro2006@gmail.com>
2026-09-25 14:19:33 -0300

    Hash da senha do candidato

--- src/main/java/br/com/arthurfloro/gestao_vagas/module/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/arthurfloro/gestao_vagas/module/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.arthurfloro.gestao_vagas.exceptions.UserFoundException;
 import br.com.arthurfloro.gestao_vagas.module.candidate.entities.CandidateEntity;
 import br.com.arthurfloro.gestao_vagas.module.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,19 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(),
                 candidateEntity.getEmail()).ifPresent(user -> {
                     throw new UserFoundException();
         });
 
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+
         return this.candidateRepository.save(candidateEntity);
+
+
     }
 }
