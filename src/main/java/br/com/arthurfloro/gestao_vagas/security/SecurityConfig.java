c6975e87be884e9097683003c12047e9b6bab057
ArthurFloro <arthur.floro2006@gmail.com>
2026-09-26 13:02:02 -0300

    Configurando PreAuthorize

--- src/main/java/br/com/arthurfloro/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/arthurfloro/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.arthurfloro.gestao_vagas.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import javax.swing.plaf.basic.BasicCheckBoxMenuItemUI;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
